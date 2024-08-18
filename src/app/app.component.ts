3d115b544e6b470076ddf9d9b73214e8c3b109b3
Filip Ivić <ivicfilip94@gmail.com>
2024-08-18 15:34:24 +0200

    remove empty scss file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { AuthService } from './core/auth/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'VehicleOverviewApp';
