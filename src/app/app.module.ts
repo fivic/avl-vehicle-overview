c966f88b0c63f7936ac45200326c86437e493e67
Filip Ivić <ivicfilip94@gmail.com>
2024-08-18 15:35:45 +0200

    add app module deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VehicleService } from './core/vehicle/vehicle.service';
+import { LoginPageComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { OverviewComponent } from './overview/overview.component';
+import { TableModule } from 'primeng/table';
+import { VehicleNamePipePipe } from './vehicle-name-pipe.pipe';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    OverviewComponent,
+    VehicleNamePipePipe,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    SkeletonModule,
+  ],
   providers: [VehicleService],
   bootstrap: [AppComponent],
 })
